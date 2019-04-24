6271c6f8c23b57a3b653a66bc720ddec8804494e
Mojtaba Dashtinejad <mojtaba@gmail.com>
2019-04-24 14:57:59 +0200

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,62 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class Media extends Component {
-  state = {
-    matches: window.matchMedia(this.props.query)
-      .matches
-  }
+function useMedia(query) {
+  let [matches, setMatches] = useState(
+    window.matchMedia(query).matches
+  )  
+  
+  useEffect(() => {
+    let media = window.matchMedia(query)
+    if (media.matches !== matches)
+        setMatches(media.matches)
 
-  componentDidMount() {
-    this.setup()
-  }
-
-  setup() {
-    let media = window.matchMedia(this.props.query)
-    if (media.matches !== this.state.matches)
-      this.setState({ matches: media.matches })
-
-    let listener = () =>
-      this.setState({ matches: media.matches })
-
+    let listener = () => setMatches(media.matches)
     media.addListener(listener)
-    this.removeListener = () =>
-      media.removeListener(listener)
-  }
+    return () => media.removeListener(listener)
+  }, [query])
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.query !== this.props.query) {
-      this.removeListener()
-      this.setup()
-    }
-  }
-
-  render() {
-    return this.props.children(this.state.matches)
-  }
+  return matches
 }
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>React Hooks</h1>
-        <Media query="(max-width: 600px)">
-          { small => (
-            <Media query="(min-width: 800px)">
-              { large => (
-                <div>
-                  <h2>Hello Media component</h2>
-                  <h3>SMALL: { small ? "YES" : "NOPE"  }</h3>
-                  <h3>LARGE: { large ? "YES" : "NOPE"  }</h3>
-                </div>
-              ) }
-            </Media>
-          ) }
-        </Media>
-      </div>
-    );
-  }
+function App() {
+  let small = useMedia("(max-width: 600px)")
+  let large = useMedia("(min-width: 800px)")
+  return (
+    <div>
+      <h1>React Hooks</h1>
+      <h2>Now using Hooks</h2>
+      <h3>SMALL: { small ? "YES" : "NOPE"  }</h3>
+      <h3>LARGE: { large ? "YES" : "NOPE"  }</h3>
+    </div>
+  )
 }
 
 export default App;
